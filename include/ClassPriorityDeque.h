0df5093acd62dcaf89bd724472688c07ef8f792f
Демиденко Егор <newercross20@gmail.com>
2026-06-10 02:56:39 +0300

    Update ClassPriorityDeque.h

--- include/ClassPriorityDeque.h
+++ include/ClassPriorityDeque.h
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+// Объявление класса
+
 /**
 * @brief Класс Очередь с приоритетом с двусторонним доступом
 */
@@ -35,14 +37,14 @@ private:
 		/**
 		* @brief Конструктор структуры по-умолчанию
 		*/
-		Element(void) : value(Type()), priority(0) {}
+		Element(void);
 
 		/**
 		* @brief Конструктор структуры по заданным значению и приоритету
 		* @param newValue - значение элемента очереди
 		* @param newPriority - приоритет элемента очереди
 		*/
-		Element(const Type& newValue, const int newPriority) : value(newValue), priority(newPriority) {}
+		Element(const Type& newValue, const int newPriority);
 	};
 
 	/**
@@ -59,442 +61,181 @@ private:
 	* @brief Выводит ошибку и завершает работу программы
 	* @param text - текст, выводимый на экран
 	*/
-	void ERROR(const string text)
-	{
-		cerr << text;
-		exit(1);
-	}
+	void ERROR(const string text);
 
 	/**
 	* @brief Возвращает индекс элемента с наименьшим приоритетом
 	* @return Индекс элемента с наименьшим приоритетом
 	*/
-	size_t getMinIndex()
-	{
-		if (count == 0)
-		{
-			ERROR("Очередь пуста");
-		}
-
-		size_t index = 0;
-		for (size_t i = 1; i < count; i++)
-		{
-			if (data[i].priority < data[index].priority)
-			{
-				index = i;
-			}
-		}
-
-		return index;
-	}
+	size_t getMinIndex();
 
 	/**
 	* @brief Возвращает индекс элемента с наибольшим приоритетом
 	* @return Индекс элемента с наибольшим приоритетом
 	*/
-	size_t getMaxIndex()
-	{
-		if (count == 0)
-		{
-			ERROR("Очередь пуста");
-		}
-
-		size_t index = 0;
-		for (size_t i = 1; i < count; i++)
-		{
-			if (data[i].priority > data[index].priority)
-			{
-				index = i;
-			}
-		}
-
-		return index;
-	}
+	size_t getMaxIndex();
 
 	/**
 	* @brief Удаляет элемент по индексу
 	* @param index - индекс удаляемого элемента
 	*/
-	void removeByIndex(const size_t index)
-	{
-		if (index >= count)
-		{
-			ERROR("Выход за границы очереди");
-		}
-
-		size_t newsize = count - 1;
-		Element* newdata = nullptr;
-
-		if (newsize > 0)
-		{
-			newdata = new Element[newsize];
-			for (size_t i = 0, j = 0; i < count; i++)
-			{
-				if (i != index)
-				{
-					newdata[j++] = data[i];
-				}
-			}
-		}
-
-		delete[] data;
-		data = newdata;
-		count = newsize;
-	}
+	void removeByIndex(const size_t index);
 
 public:
 	/**
 	* @brief Конструктор класса по-умолчанию, задаёт пустую очередь с приоритетом
 	*/
-	PriorityDeque(void) : data(nullptr), count(0) {}
+	PriorityDeque(void);
 
 	/**
 	* @brief Конструктор класса, задаёт очередь с приоритетом по списку инициализации
 	* @param other - список пар значение-приоритет
 	*/
-	PriorityDeque(initializer_list<pair<Type, int>> other) : data(nullptr), count(other.size())
-	{
-		if (count > 0)
-		{
-			data = new Element[count];
-			for (size_t i = 0; const auto& value : other)
-			{
-				data[i++] = Element(value.first, value.second);
-			}
-		}
-	}
+	PriorityDeque(initializer_list<pair<Type, int>> other);
 
 	/**
 	* @brief Конструктор класса, задаёт очередь с приоритетом по переданному списку
 	* @param other - список пар значение-приоритет
 	*/
-	PriorityDeque(list<pair<Type, int>>& other) : data(nullptr), count(other.size())
-	{
-		if (count > 0)
-		{
-			data = new Element[count];
-			for (size_t i = 0; const auto& value : other)
-			{
-				data[i++] = Element(value.first, value.second);
-			}
-		}
-	}
+	PriorityDeque(list<pair<Type, int>>& other);
 
 	/**
 	* @brief Конструктор класса, задаёт очередь с приоритетом по переданной очереди
 	* @param other - переданная очередь с приоритетом
 	*/
-	PriorityDeque(const PriorityDeque& other) : data(nullptr), count(other.count)
-	{
-		if (count > 0)
-		{
-			data = new Element[count];
-			for (size_t i = 0; i < count; i++)
-			{
-				data[i] = other.data[i];
-			}
-		}
-	}
+	PriorityDeque(const PriorityDeque& other);
 
 	/**
 	* @brief Конструктор класса, задаёт очередь с приоритетом по ссылке на очередь
 	* @param other - ссылка на переданную очередь с приоритетом
 	*/
-	PriorityDeque(PriorityDeque&& other) : data(other.data), count(other.count)
-	{
-		other.data = nullptr;
-		other.count = 0;
-	}
+	PriorityDeque(PriorityDeque&& other);
 
 	/**
 	* @brief Добавление элемента в очередь с приоритетом
 	* @param object - объект добавляемый в очередь
 	* @param priority - приоритет добавляемого объекта
 	*/
-	void push(const Type& object, const int priority)
-	{
-		Element* newdata = new Element[count + 1];
-
-		for (size_t i = 0; i < count; i++)
-		{
-			newdata[i] = data[i];
-		}
-
-		newdata[count] = Element(object, priority);
-
-		delete[] data;
-		data = newdata;
-		count++;
-	}
+	void push(const Type& object, const int priority);
 
 	/**
 	* @brief Удаление элемента с наименьшим приоритетом
 	*/
-	void pop_min()
-	{
-		if (count > 0)
-		{
-			removeByIndex(getMinIndex());
-		}
-		else
-		{
-			cout << "Очередь пуста, удаление не выполнено" << endl;
-		}
-	}
+	void pop_min();
 
 	/**
 	* @brief Удаление элемента с наибольшим приоритетом
 	*/
-	void pop_max()
-	{
-		if (count > 0)
-		{
-			removeByIndex(getMaxIndex());
-		}
-		else
-		{
-			cout << "Очередь пуста, удаление не выполнено" << endl;
-		}
-	}
+	void pop_max();
 
 	/**
 	* @brief Поиск элемента с наименьшим приоритетом
 	* @return Элемент с наименьшим приоритетом
 	*/
-	Type peek_min()
-	{
-		return data[getMinIndex()].value;
-	}
+	Type peek_min();
 
 	/**
 	* @brief Поиск элемента с наибольшим приоритетом
 	* @return Элемент с наибольшим приоритетом
 	*/
-	Type peek_max()
-	{
-		return data[getMaxIndex()].value;
-	}
+	Type peek_max();
 
 	/**
 	* @brief Поиск наименьшего приоритета
 	* @return Наименьший приоритет
 	*/
-	int priority_min()
-	{
-		return data[getMinIndex()].priority;
-	}
+	int priority_min();
 
 	/**
 	* @brief Поиск наибольшего приоритета
 	* @return Наибольший приоритет
 	*/
-	int priority_max()
-	{
-		return data[getMaxIndex()].priority;
-	}
+	int priority_max();
 
 	/**
 	* @brief Проверка очереди с приоритетом на пустоту
 	* @return true или false
 	*/
-	bool empty() const
-	{
-		return count == 0;
-	}
+	bool empty() const;
 
 	/**
 	* @brief Очистка очереди с приоритетом
 	*/
-	void clear()
-	{
-		delete[] data;
-		data = nullptr;
-		count = 0;
-	}
+	void clear();
 
 	/**
 	* @brief Вывод размера очереди с приоритетом
 	* @return Число - размер очереди с приоритетом
 	*/
-	size_t printSize() const
-	{
-		return count;
-	}
+	size_t printSize() const;
 
 	/**
 	* @brief Вывод данных очереди с приоритетом
 	* @return Указатель на область памяти, где хранятся данные очереди
 	*/
-	const void* printData() const
-	{
-		return data;
-	}
+	const void* printData() const;
 
 	/**
 	* @brief Вывод размера очереди с приоритетом
 	* @param os - поток вывода
 	* @return Текст
 	*/
-	ostream& printSize(ostream& os) const
-	{
-		if (count == 0)
-		{
-			os << "Очередь пуста!" << endl;
-		}
-		else
-		{
-			os << "Размер очереди: " << count << endl;
-		}
-
-		return os;
-	}
+	ostream& printSize(ostream& os) const;
 
 	/**
 	* @brief Вывод очереди с приоритетом в строку
 	* @return Очередь с приоритетом, сериализованная в строку
 	*/
-	string toString() const
-	{
-		stringstream stream;
-		stream << "{";
-		for (size_t i = 0; i < count; i++)
-		{
-			stream << "(" << data[i].value << "; " << data[i].priority << ")";
-			if (i != count - 1)
-			{
-				stream << ", ";
-			}
-		}
-		stream << "}";
-		return stream.str();
-	}
+	string toString() const;
 
 	/**
 	* @brief Оператор присваивания очереди с приоритетом
 	* @param other - другая очередь с приоритетом
 	* @return Очередь с приоритетом
 	*/
-	PriorityDeque& operator = (const PriorityDeque& other)
-	{
-		if (this != &other)
-		{
-			delete[] data;
-
-			count = other.count;
-			data = nullptr;
-
-			if (count > 0)
-			{
-				data = new Element[count];
-				for (size_t i = 0; i < count; i++)
-				{
-					data[i] = other.data[i];
-				}
-			}
-		}
-
-		return *this;
-	}
+	PriorityDeque& operator=(const PriorityDeque& other);
 
 	/**
 	* @brief Оператор присваивания очереди с приоритетом
 	* @param other - ссылка на другую очередь с приоритетом
 	* @return Очередь с приоритетом
 	*/
-	PriorityDeque& operator = (PriorityDeque&& other)
-	{
-		if (this != &other)
-		{
-			delete[] data;
-
-			data = other.data;
-			count = other.count;
-
-			other.data = nullptr;
-			other.count = 0;
-		}
-
-		return *this;
-	}
+	PriorityDeque& operator=(PriorityDeque&& other);
 
 	/**
 	* @brief Оператор сравнения очередей с приоритетом
 	* @param other - другая очередь с приоритетом
 	* @return true или false
 	*/
-	bool operator == (const PriorityDeque& other) const
-	{
-		if (count != other.count)
-		{
-			return false;
-		}
-
-		for (size_t i = 0; i < count; i++)
-		{
-			if (data[i].value != other.data[i].value || data[i].priority != other.data[i].priority)
-			{
-				return false;
-			}
-		}
-
-		return true;
-	}
+	bool operator==(const PriorityDeque& other) const;
 
 	/**
 	* @brief Оператор сравнения очередей с приоритетом
 	* @param other - другая очередь с приоритетом
 	* @return true или false
 	*/
-	bool operator != (const PriorityDeque& other) const
-	{
-		return !(*this == other);
-	}
+	bool operator!=(const PriorityDeque& other) const;
 
 	/**
 	* @brief Оператор, показывающий n-й элемент очереди с приоритетом
 	* @param index - индекс элемента, который будет показан
 	* @return n-й элемент очереди с приоритетом
 	*/
-	Type& operator [] (const size_t index)
-	{
-		if (index < count)
-		{
-			return data[index].value;
-		}
-
-		ERROR("Выход за границы очереди");
-	}
+	Type& operator[](const size_t index);
 
 	/**
 	* @brief Оператор, показывающий n-й элемент очереди с приоритетом
 	* @param index - индекс элемента, который будет показан
 	* @return n-й элемент очереди с приоритетом
 	*/
-	const Type& operator [] (const size_t index) const
-	{
-		if (index < count)
-		{
-			return data[index].value;
-		}
-
-		cerr << "Выход за границы очереди";
-		exit(1);
-	}
+	const Type& operator[](const size_t index) const;
 
 	/**
 	* @brief Вывод приоритета n-го элемента очереди с приоритетом
 	* @param index - индекс элемента, приоритет которого будет показан
 	* @return Приоритет n-го элемента очереди с приоритетом
 	*/
-	int getPriority(const size_t index)
-	{
-		if (index < count)
-		{
-			return data[index].priority;
-		}
-
-		ERROR("Выход за границы очереди");
-	}
+	int getPriority(const size_t index);
 
 	/**
 	* @brief Оператор вывода очереди с приоритетом в поток вывода
@@ -502,7 +243,7 @@ public:
 	* @param other - очередь с приоритетом
 	* @return Очередь с приоритетом, сериализованная в строку
 	*/
-	friend ostream& operator << (ostream& os, PriorityDeque& other)
+	friend ostream& operator<<(ostream& os, PriorityDeque& other)
 	{
 		os << other.toString() << endl;
 		return os;
@@ -514,7 +255,7 @@ public:
 	* @param other - очередь с приоритетом
 	* @return Поток ввода
 	*/
-	friend istream& operator >> (istream& is, PriorityDeque& other)
+	friend istream& operator>>(istream& is, PriorityDeque& other)
 	{
 		int inputCount = 0;
 		cout << "Введите количество элементов в очереди: ";
@@ -558,8 +299,375 @@ public:
 	/**
 	* @brief Деструктор класса по-умолчанию
 	*/
-	~PriorityDeque()
+	~PriorityDeque();
+};
+
+//Реализация класса
+
+
+template<typename Type>
+PriorityDeque<Type>::Element::Element(void) : value(Type()), priority(0) {}
+
+template<typename Type>
+PriorityDeque<Type>::Element::Element(const Type& newValue, const int newPriority)
+	: value(newValue), priority(newPriority) {}
+
+
+template<typename Type>
+void PriorityDeque<Type>::ERROR(const string text)
+{
+	cerr << text;
+	exit(1);
+}
+
+template<typename Type>
+size_t PriorityDeque<Type>::getMinIndex()
+{
+	if (count == 0)
+	{
+		ERROR("Очередь пуста");
+	}
+
+	size_t index = 0;
+	for (size_t i = 1; i < count; i++)
+	{
+		if (data[i].priority < data[index].priority)
+		{
+			index = i;
+		}
+	}
+
+	return index;
+}
+
+template<typename Type>
+size_t PriorityDeque<Type>::getMaxIndex()
+{
+	if (count == 0)
+	{
+		ERROR("Очередь пуста");
+	}
+
+	size_t index = 0;
+	for (size_t i = 1; i < count; i++)
+	{
+		if (data[i].priority > data[index].priority)
+		{
+			index = i;
+		}
+	}
+
+	return index;
+}
+
+template<typename Type>
+void PriorityDeque<Type>::removeByIndex(const size_t index)
+{
+	if (index >= count)
+	{
+		ERROR("Выход за границы очереди");
+	}
+
+	size_t newsize = count - 1;
+	Element* newdata = nullptr;
+
+	if (newsize > 0)
+	{
+		newdata = new Element[newsize];
+		for (size_t i = 0, j = 0; i < count; i++)
+		{
+			if (i != index)
+			{
+				newdata[j++] = data[i];
+			}
+		}
+	}
+
+	delete[] data;
+	data = newdata;
+	count = newsize;
+}
+
+
+template<typename Type>
+PriorityDeque<Type>::PriorityDeque(void) : data(nullptr), count(0) {}
+
+template<typename Type>
+PriorityDeque<Type>::PriorityDeque(initializer_list<pair<Type, int>> other)
+	: data(nullptr), count(other.size())
+{
+	if (count > 0)
+	{
+		data = new Element[count];
+		for (size_t i = 0; const auto& value : other)
+		{
+			data[i++] = Element(value.first, value.second);
+		}
+	}
+}
+
+template<typename Type>
+PriorityDeque<Type>::PriorityDeque(list<pair<Type, int>>& other)
+	: data(nullptr), count(other.size())
+{
+	if (count > 0)
+	{
+		data = new Element[count];
+		for (size_t i = 0; const auto& value : other)
+		{
+			data[i++] = Element(value.first, value.second);
+		}
+	}
+}
+
+template<typename Type>
+PriorityDeque<Type>::PriorityDeque(const PriorityDeque& other)
+	: data(nullptr), count(other.count)
+{
+	if (count > 0)
+	{
+		data = new Element[count];
+		for (size_t i = 0; i < count; i++)
+		{
+			data[i] = other.data[i];
+		}
+	}
+}
+
+template<typename Type>
+PriorityDeque<Type>::PriorityDeque(PriorityDeque&& other)
+	: data(other.data), count(other.count)
+{
+	other.data = nullptr;
+	other.count = 0;
+}
+
+template<typename Type>
+void PriorityDeque<Type>::push(const Type& object, const int priority)
+{
+	Element* newdata = new Element[count + 1];
+
+	for (size_t i = 0; i < count; i++)
+	{
+		newdata[i] = data[i];
+	}
+
+	newdata[count] = Element(object, priority);
+
+	delete[] data;
+	data = newdata;
+	count++;
+}
+
+template<typename Type>
+void PriorityDeque<Type>::pop_min()
+{
+	if (count > 0)
+	{
+		removeByIndex(getMinIndex());
+	}
+	else
+	{
+		cout << "Очередь пуста, удаление не выполнено" << endl;
+	}
+}
+
+template<typename Type>
+void PriorityDeque<Type>::pop_max()
+{
+	if (count > 0)
+	{
+		removeByIndex(getMaxIndex());
+	}
+	else
+	{
+		cout << "Очередь пуста, удаление не выполнено" << endl;
+	}
+}
+
+template<typename Type>
+Type PriorityDeque<Type>::peek_min()
+{
+	return data[getMinIndex()].value;
+}
+
+template<typename Type>
+Type PriorityDeque<Type>::peek_max()
+{
+	return data[getMaxIndex()].value;
+}
+
+template<typename Type>
+int PriorityDeque<Type>::priority_min()
+{
+	return data[getMinIndex()].priority;
+}
+
+template<typename Type>
+int PriorityDeque<Type>::priority_max()
+{
+	return data[getMaxIndex()].priority;
+}
+
+template<typename Type>
+bool PriorityDeque<Type>::empty() const
+{
+	return count == 0;
+}
+
+template<typename Type>
+void PriorityDeque<Type>::clear()
+{
+	delete[] data;
+	data = nullptr;
+	count = 0;
+}
+
+template<typename Type>
+size_t PriorityDeque<Type>::printSize() const
+{
+	return count;
+}
+
+template<typename Type>
+const void* PriorityDeque<Type>::printData() const
+{
+	return data;
+}
+
+template<typename Type>
+ostream& PriorityDeque<Type>::printSize(ostream& os) const
+{
+	if (count == 0)
+	{
+		os << "Очередь пуста!" << endl;
+	}
+	else
+	{
+		os << "Размер очереди: " << count << endl;
+	}
+
+	return os;
+}
+
+template<typename Type>
+string PriorityDeque<Type>::toString() const
+{
+	stringstream stream;
+	stream << "{";
+	for (size_t i = 0; i < count; i++)
+	{
+		stream << "(" << data[i].value << "; " << data[i].priority << ")";
+		if (i != count - 1)
+		{
+			stream << ", ";
+		}
+	}
+	stream << "}";
+	return stream.str();
+}
+
+template<typename Type>
+PriorityDeque<Type>& PriorityDeque<Type>::operator=(const PriorityDeque& other)
+{
+	if (this != &other)
 	{
 		delete[] data;
+
+		count = other.count;
+		data = nullptr;
+
+		if (count > 0)
+		{
+			data = new Element[count];
+			for (size_t i = 0; i < count; i++)
+			{
+				data[i] = other.data[i];
+			}
+		}
 	}
-};
+
+	return *this;
+}
+
+template<typename Type>
+PriorityDeque<Type>& PriorityDeque<Type>::operator=(PriorityDeque&& other)
+{
+	if (this != &other)
+	{
+		delete[] data;
+
+		data = other.data;
+		count = other.count;
+
+		other.data = nullptr;
+		other.count = 0;
+	}
+
+	return *this;
+}
+
+template<typename Type>
+bool PriorityDeque<Type>::operator==(const PriorityDeque& other) const
+{
+	if (count != other.count)
+	{
+		return false;
+	}
+
+	for (size_t i = 0; i < count; i++)
+	{
+		if (data[i].value != other.data[i].value || data[i].priority != other.data[i].priority)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template<typename Type>
+bool PriorityDeque<Type>::operator!=(const PriorityDeque& other) const
+{
+	return !(*this == other);
+}
+
+template<typename Type>
+Type& PriorityDeque<Type>::operator[](const size_t index)
+{
+	if (index < count)
+	{
+		return data[index].value;
+	}
+
+	ERROR("Выход за границы очереди");
+}
+
+template<typename Type>
+const Type& PriorityDeque<Type>::operator[](const size_t index) const
+{
+	if (index < count)
+	{
+		return data[index].value;
+	}
+
+	cerr << "Выход за границы очереди";
+	exit(1);
+}
+
+template<typename Type>
+int PriorityDeque<Type>::getPriority(const size_t index)
+{
+	if (index < count)
+	{
+		return data[index].priority;
+	}
+
+	ERROR("Выход за границы очереди");
+}
+
+template<typename Type>
+PriorityDeque<Type>::~PriorityDeque()
+{
+	delete[] data;
+}
